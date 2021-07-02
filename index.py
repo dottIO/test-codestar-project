017155c813b6c4bc75b2064bd9f8627c499ad4e6
Takeshi Haga <13972608+hagaji@users.noreply.github.com>
2021-07-02 22:58:57 +0900

    update output

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Oh!Yes. Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
